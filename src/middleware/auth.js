8c4741a7f1e289d1dd6d0addec060e069a22e33c
Michal Svoboda <misak.svobo@gmail.com>
2020-05-07 21:52:44 +0200

    fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,9 +5,9 @@ const auth = async(req, res, next) => {
     try {
             const token = req.header('Authorization').replace('Bearer ', '')
             console.log('token>'+token)
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            const user = await User.findOne({'_id': decoded._id, 'tokens.token': token})
             console.log(process.env.JWT_SECRET)
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const user = await User.findOne({'_id': decoded._id, 'tokens.token': token})          
 
             if (!user) {
                 throw new Error('Unknown user')

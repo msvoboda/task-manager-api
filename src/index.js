fe3bd6a4f6c34ae8257e2c7fb64e802cb0236105
Michal Svoboda <misak.svobo@gmail.com>
2020-05-07 21:56:55 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -18,11 +18,12 @@ const Task = require('./models/task')
 const User = require('./models/user')
 
 const main = async () => {
+    /*
     const user = await User.findById('5e999bbadbfa4c4f50894e89')
     if (!user) {
         await user.populate('tasks').execPopulate()
         console.log(user.tasks)
-    }
+    }*/
 }
 
 main()
